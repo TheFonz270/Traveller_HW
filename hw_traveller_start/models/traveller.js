5e5221efd62f2b99638c59b745ebd5331812d5cb
Martin Quinn <thefonz270@hotmail.com>
2021-11-24 16:47:06 +0000

    MVP and extensions complete

--- hw_traveller_start/models/traveller.js
+++ hw_traveller_start/models/traveller.js
@@ -3,27 +3,34 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+const result = this.journeys.map(a => a.startLocation);
+return result;
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  const result = this.journeys.map(a => a.endLocation);
+  return result;
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  let result = this.journeys.filter(a => a.transport == transport)
+  return result;
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  let result = this.journeys.filter(a => a.distance > minDistance);
+  return result;
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  let result = this.journeys.reduce((previousValue, currentValue) => previousValue + currentValue.distance, 0);
+  return result;
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  const transports = this.journeys.map(a => a.transport);
+  const result = transports.filter((item, index) => transports.indexOf(item) === index);
+  return result;
 };
 
 
